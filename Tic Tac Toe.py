9bd0ee990159fa77e06358c77339327614654678
Simon <girmasimon95@gmail.com>
2025-07-03 14:20:29 +0400

    going to be making a tic tac toe game using python

---
+++ Tic Tac Toe.py
@@ -0,0 +1,22 @@
+board =["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+current_player = "X"
+winner = None
+gameRunning = True
+#game board
+def printBoard(board):
+    print(" " + board[0] + " | " + board[1] + " | " + board[2])
+    print("---|---|---")
+    print(" " + board[3] + " | " + board[4] + " | " + board[5])
+    print("---|---|---")
+    print(" " + board[6] + " | " + board[7] + " | " + board[8])
+printBoard(board)
+#take player input
+
+#check for win, tie or lose
+
+#switch the player
+
+#check for win or tie again
+
